115c63705065868d3b1273fbe255b41e16e9a0c1
Andrey Sabitov <andrey.sabitoff@yandex.ru>
2025-02-27 13:17:02 +0500

    Добавить AnalyzerRunner

--- telemetry/analyzer/src/main/java/ru/practicum/analyzer/AnalyzerApp.java
+++ telemetry/analyzer/src/main/java/ru/practicum/analyzer/AnalyzerApp.java
@@ -2,21 +2,10 @@ package ru.practicum.analyzer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import ru.practicum.analyzer.processors.HubEventProcessor;
-import ru.practicum.analyzer.processors.SnapshotProcessor;
 
 @SpringBootApplication
 public class AnalyzerApp {
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(AnalyzerApp.class, args);
-        HubEventProcessor hubEventProcessor = context.getBean(HubEventProcessor.class);
-        SnapshotProcessor snapshotProcessor = context.getBean(SnapshotProcessor.class);
-
-        Thread hubEventsThread = new Thread(hubEventProcessor);
-        hubEventsThread.setName("HubEventHandlerThread");
-        hubEventsThread.start();
-
-        snapshotProcessor.start();
+        SpringApplication.run(AnalyzerApp.class, args);
     }
 }
